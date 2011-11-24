1be065c433f23f0d5d2cb9d549db4e83040b78df
Grzegorz Dziemidowicz <dziemid@rybnet.pl>
2011-11-24 22:57:13 +0100

    Removed redundant assertion

--- java/string_calculator/src/test/java/StringCalculatorTest.java
+++ java/string_calculator/src/test/java/StringCalculatorTest.java
@@ -7,7 +7,6 @@ public class StringCalculatorTest {
     @Test
     public void testCalculator() throws Exception {
         StringCalculator s = new StringCalculator();
-        assertEquals(0, s.add("")); //should treat empty string as zero
         assertEquals(17, s.add("17")); // should add single numbers
         assertEquals(2356, s.add("2341,15")); // should add comma separated numbers
         assertEquals(35, s.add("34\n1")); // should treat new line as separator
