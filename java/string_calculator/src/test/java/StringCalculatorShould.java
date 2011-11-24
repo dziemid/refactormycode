0c65debd02632b7738a84d507faa0d52b2c05375
Grzegorz Dziemidowicz <dziemid@rybnet.pl>
2011-11-24 23:08:44 +0100

    Got rid of should in test names

--- java/string_calculator/src/test/java/StringCalculatorShould.java
+++ java/string_calculator/src/test/java/StringCalculatorShould.java
@@ -5,31 +5,31 @@ import static junit.framework.Assert.assertEquals;
 public class StringCalculatorShould {
 
     @Test
-    public void should_treat_empty_string_as_zero() throws Exception {
+    public void treat_empty_string_as_zero() throws Exception {
         StringCalculator s = new StringCalculator();
         assertEquals(0, s.add(""));
     }
 
     @Test
-    public void should_add_single_numbers() throws Exception {
+    public void add_single_numbers() throws Exception {
         StringCalculator s = new StringCalculator();
         assertEquals(3, s.add("3"));
     }
 
     @Test
-    public void should_add_comma_separated_numbers() throws Exception {
+    public void add_comma_separated_numbers() throws Exception {
         StringCalculator s = new StringCalculator();
         assertEquals(3, s.add("1,2"));
     }
 
     @Test
-    public void should_treat_new_line_as_separator() throws Exception {
+    public void treat_new_line_as_separator() throws Exception {
         StringCalculator s = new StringCalculator();
         assertEquals(9, s.add("4\n5"));
     }
 
     @Test
-    public void should_support_custom_separators() throws Exception {
+    public void support_custom_separators() throws Exception {
         StringCalculator s = new StringCalculator();
         assertEquals(7, s.add("//;\n5;2"));
     }
