3358e19bee342cf0e2080a5d4041afdf1d7fce92
Grzegorz Dziemidowicz <dziemid@rybnet.pl>
2011-11-24 23:00:04 +0100

    One assertion per test

--- java/string_calculator/src/test/java/StringCalculatorTest.java
+++ java/string_calculator/src/test/java/StringCalculatorTest.java
@@ -5,12 +5,27 @@ import static junit.framework.Assert.assertEquals;
 public class StringCalculatorTest {
 
     @Test
-    public void testCalculator() throws Exception {
+    public void should_add_single_numbers() throws Exception {
         StringCalculator s = new StringCalculator();
-        assertEquals(17, s.add("17")); // should add single numbers
-        assertEquals(2356, s.add("2341,15")); // should add comma separated numbers
-        assertEquals(35, s.add("34\n1")); // should treat new line as separator
-        assertEquals(7, s.add("//;\n5;2")); // should support custom separators
+        assertEquals(17, s.add("17"));
+    }
+
+    @Test
+    public void should_add_comma_separated_numbers() throws Exception {
+        StringCalculator s = new StringCalculator();
+        assertEquals(2356, s.add("2341,15"));
+    }
+
+    @Test
+    public void should_treat_new_line_as_separator() throws Exception {
+        StringCalculator s = new StringCalculator();
+        assertEquals(35, s.add("34\n1"));
+    }
+
+    @Test
+    public void should_support_custom_separators() throws Exception {
+        StringCalculator s = new StringCalculator();
+        assertEquals(7, s.add("//;\n5;2"));
     }
 
     @Test
